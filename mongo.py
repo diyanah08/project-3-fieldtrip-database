a83229aef32c27fd07d0dfef193b65c4fcdfdc2f
Ubuntu <ubuntu@ip-172-31-86-83.ec2.internal>
2019-10-23 08:51:27 +0000

    added add_new_data form

--- mongo.py
+++ mongo.py
@@ -29,6 +29,49 @@ def indexTest():
 def landingPage():
     return render_template('landing_page.template.html')
 
+@app.route('/add')
+def addForm():
+    themes = ["Occupation", "Nature", "Conservation", "Museums", "Others"]
+    age = ["N1", "N2", "K1","K2", "All"]
+    prices = ["Free", "Paid"]
+    
+    return render_template('add_new.template.html', themes=themes, age=age, prices=prices)
+
+@app.route('/add', methods=['POST'])
+def add():
+    image = request.files.get('image') #1 -- get the uploaded image
+    filename = images_upload_set.save(image)
+    name = request.form.get('name')
+    address = request.form['address']
+    email = request.form['contact']
+    description = request.form['description']
+    activities = request.form.get('activities')
+    activitiesArr = [x.strip() for x in activities.split(',')]
+    theme = request.form.getlist('theme')
+    age_group = request.form.getlist('age-group')
+    price = request.form.get('price')
+    
+    themes = ["Occupation", "Nature", "Conservation", "Museums", "Others"]
+    age = ["N1", "N2", "K1","K2", "All"]
+    prices = ["Free", "Paid"]
+    
+    coll.insert({
+        "name" : name,
+        "address" : address,
+        "email": email,
+        "description": description,
+        "activities": activitiesArr,
+        "themes": theme,
+        "age_group": age_group,
+        "price": price,
+        "image": {
+                'image_url' : images_upload_set.url(filename)
+            }
+    })
+    
+    return render_template('add_new.template.html', themes=themes, age=age, prices=prices, image=image, filename=filename, name=name, address=address, email=email, description=description, activities=activities, theme=theme, age_group=age_group, price=price)
+
+
 if __name__ == '__main__':
 	app.run(host=os.environ.get('IP'),
 	port=int(os.environ.get('PORT')),
